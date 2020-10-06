511457084a1211900b81be1d49a4a7257a87ec49
Marcos Vinicios <viniciosdinis@gmail.com>
2020-10-06 22:59:51 +0100

    final code

---
+++ UserSecrets/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace UserSecrets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IOptions<Security> _config;
+
+        public HomeController(IOptions<Security> config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var security = _config.Value;
+
+            return Ok(security);
+        }
+    }
+}
